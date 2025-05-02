9309b0155ffc59e1946e009a7e641151f6d9495b
Nimra-Bilal <yoyo123000001@gmail.com>
2025-05-03 00:20:12 +0500

    connection

--- server/configs/Mongodb.js
+++ server/configs/Mongodb.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   try {
     mongoose.connection.on('connected', () => console.log('Database CONNECTED'));
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`Connected to: ${process.env.MONGODB_URI}`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`);
+    console.log(`Connected to: ${process.env.MONGODB_URI}/lms`);
   } catch (err) {
     console.error('DB Connection Error:', err);
     process.exit(1); // Crash app if DB fails
