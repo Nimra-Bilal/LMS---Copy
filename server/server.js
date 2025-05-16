fb545dd7997813ae6542712e6dfcb91500a6277c
Nimra-Bilal <yoyo123000001@gmail.com>
2025-05-16 20:09:17 +0500

    mm

--- server/server.js
+++ server/server.js
@@ -13,11 +13,13 @@ const app = express();
 
   // Connect to DB
   await connectDb();
-  await connectCloudinary();
-
-  // Middleware
+  await connectCloudinary(); 
   app.use(cors());
   app.use(clerkMiddleware());
+  app.post('/stripe',express.raw({type:'application/json'}) ,stripeWebhooks )
+
+  // Middleware
+ 
   app.use(express.json());
 
   // Routes
@@ -26,7 +28,7 @@ const app = express();
   app.use('/api/educator' , clerkMiddleware() ,  educatorRouter)
   app.use('/api/course',express.json(), courseRouter)
   app.use('/api/user',express.json(), userRouter)
-  app.post('/stripe',express.raw({type:'application/json'}) ,stripeWebhooks )
+  // app.post('/stripe',express.raw({type:'application/json'}) ,stripeWebhooks )
 
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () =>{ console.log(`Server running on port ${PORT}`)});
